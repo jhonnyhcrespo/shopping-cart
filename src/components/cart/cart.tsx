680eef8df35d33f70804ac4ae6d57b4d9ecde2c9
Jhonny Crespo <jhonnyh.crpo@gmail.com>
2025-07-08 15:11:08 -0400

    Read cart data from /api/carts

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -1,13 +1,50 @@
-import { Box, Stack } from '@chakra-ui/react'
-import React from 'react'
+"use client";
+
+import { Box, Stack, Text } from '@chakra-ui/react'
+import React, { useState, useEffect } from 'react'
 import { CartItem } from './cart-item'
 import { CartTotalSummary } from './cart-total-summary'
-// TODO: Read from API
-import { carts } from '@/lib/data/carts'
+import { CartsResponse, Cart as CartType } from '@/lib/types'
+
+const getCart = async (): Promise<CartType | null> => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_CART_API_BASE_URL}/api/carts?customerId=cust_001`
+    )
+
+    if (!res.ok) {
+      console.error("Failed to fetch cart:", res.status, res.statusText)
+      return null
+    }
+
+    const resBody: CartsResponse = await res.json();
+    return resBody.carts.length > 0 ? resBody.carts[0] : null;
+  } catch (error) {
+    console.error("Failed to fetch cart:", error);
+    return null
+  }
+};
 
 export const Cart = () => {
 
-  const cart = carts[0]
+  const [cart, setCart] = useState<CartType | null>()
+
+  // TODO: Add cache
+  useEffect(() => {
+    const fetchCart = async () => {
+      const cart = await getCart();
+      setCart(cart)
+    }
+    fetchCart()
+  }, [])
+
+  if (cart == null || cart.items.length === 0) {
+    return (
+      <Box>
+        <Text>Your cart is empty</Text>
+      </Box>
+    )
+  }
 
   return (
     <Stack id="cart" data-testid="cart" height="full">
